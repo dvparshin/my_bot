725ddffa86ab51fbf3892a4edad169e81617bd2b
Dmitriy Parshin <parshyn.dima@gmail.com>
2024-09-11 13:13:46 +0300

    first commit

--- bot.py
+++ bot.py
@@ -1,26 +1,29 @@
-import os
 import logging
-from telegram import Update
-from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.DEBUG
-)
+import settings
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Здравствуй, пользователь!")
+logging.basicConfig(filename="bot.log", level=logging.INFO)
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+def greet_user(update, context):
+    print("Вызван /start")
+    update.message.reply_text("Здравствуй, пользователь")
 
-if __name__ == '__main__':
-    application = ApplicationBuilder().token(os.environ['API_KEY']).build()
-    
-    start_handler = CommandHandler('start', start)
-    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+def talk_to_me(update, context):
+    text = update.message.text
+    print(text)
+    update.message.reply_text(text)
 
-    application.add_handler(start_handler)
-    application.add_handler(echo_handler)
+def main():
+    mybot = Updater(settings.API_KEY)
 
-    application.run_polling()
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+
+    logging.info("Bot starting")
+    mybot.start_polling()
+    mybot.idle()
+
+if __name__ == "__main__":
+    main()
